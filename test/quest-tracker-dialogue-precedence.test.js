c6a4a313178ab151cc70372970bd9582a728e88a
Eric Londaits <eric.londaits@gmail.com>
2024-04-22 20:06:56 -0300

    test: Add tests for quest dialogue precedence.

---
+++ test/quest-tracker-dialogue-precedence.test.js
@@ -0,0 +1,74 @@
+const chai = require('chai');
+const FlagStore = require('../src/js/lib/model/flag-store');
+const QuestTracker = require('../src/js/lib/model/quest-tracker');
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+const DialogueIterator = require('../src/js/lib/model/dialogues/dialogue-iterator');
+const TestDialogueIteratorContext = require('./lib/test-dialogue-iterator-context');
+const getDialogueTrace = require('./lib/get-dialogue-trace');
+
+const { expect } = chai;
+
+function loadFixture(name) {
+  return yaml.load(fs.readFileSync(path.join(__dirname, 'fixtures', 'quest-tracker', name)));
+}
+
+describe('QuestTracker dialogue precedence', () => {
+  let questTracker = null;
+  let flags = null;
+  const fixture = loadFixture('precedence.yml');
+  const config = {
+    game: {
+      maxActiveQuests: 2,
+    },
+    storylines: {
+      default: fixture,
+    },
+  };
+
+  beforeEach(() => {
+    flags = new FlagStore();
+    questTracker = new QuestTracker(config, flags);
+    questTracker.setActiveStoryline('default');
+  });
+
+  it('should default to the storyline dialogue', () => {
+    const dialogue = questTracker.getNpcDialogue('boy');
+    const context = new TestDialogueIteratorContext();
+    const iterator = new DialogueIterator(dialogue, context);
+    expect(getDialogueTrace(iterator)).to.deep.equal(['I like chocolate.']);
+  });
+
+  it('should use the "available" dialogue if there is one', () => {
+    const dialogue = questTracker.getNpcDialogue('mayor');
+    const context = new TestDialogueIteratorContext();
+    const iterator = new DialogueIterator(dialogue, context);
+    expect(getDialogueTrace(iterator)).to.deep.equal(['Help me find out if everyone likes milk chocolate.']);
+  });
+
+  it('should not use the "available" dialogue if the quest is not available', () => {
+    flags.set('quest.milkChocolate.done', true);
+    const dialogue = questTracker.getNpcDialogue('mayor');
+    const context = new TestDialogueIteratorContext();
+    const iterator = new DialogueIterator(dialogue, context);
+    expect(getDialogueTrace(iterator)).to.deep.equal(['Does everyone like chocolate?']);
+  });
+
+  it('should use the quest dialogue if there is one', () => {
+    questTracker.setActiveQuest('milkChocolate');
+    const dialogue = questTracker.getNpcDialogue('chocolateLover');
+    const context = new TestDialogueIteratorContext();
+    const iterator = new DialogueIterator(dialogue, context);
+    expect(getDialogueTrace(iterator)).to.deep.equal(['I haven\'t tried milk chocolate.']);
+  });
+
+  it('should use the stage dialogue if there is one', () => {
+    questTracker.setActiveQuest('milkChocolate');
+    flags.set('hasTriedMilkChocolate', true);
+    const dialogue = questTracker.getNpcDialogue('chocolateLover');
+    const context = new TestDialogueIteratorContext();
+    const iterator = new DialogueIterator(dialogue, context);
+    expect(getDialogueTrace(iterator)).to.deep.equal(['I love milk chocolate.']);
+  });
+});
